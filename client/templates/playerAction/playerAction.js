6c3a4773da76dfb378fbaf767f14ad7d42a7aff8
boxcee <jaeckiezucker@gmail.com>
2014-10-27 18:49:46 +0100

    Added addAction Meteor.method

--- client/templates/playerAction/playerAction.js
+++ client/templates/playerAction/playerAction.js
@@ -5,10 +5,37 @@ Template.addAction.helpers({
     }
 });
 
+Template.displayAction.helpers({
+    userAction: function() {
+        return playerActions.find();
+    }
+});
+
 $(function() {
     $('#input-tags').selectize({
         delimiter: ',',
         persist: false,
         create: true
     });
+});
+
+Template.addAction.events({
+    'click #offen': function(e) {
+        e.preventDefault();
+
+        var user = Meteor.user();
+
+        var actionContent = {
+            action: document.getElementById('action'),
+            actors: user.username + " " + document.getElementById('actors'),
+            time: new Date().now,
+            type: "offen"
+        };
+
+        Meteor.call('addAction', actionContent, function(err, result) {
+            if (err)
+                alert(err.reason)
+        });
+
+    }
 });
